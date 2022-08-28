3cff0bd0260cb91ebe9632f97dd2099f01d23660
Matthew Batchelder <borkweb@gmail.com>
2022-08-28 00:28:07 -0400

    Default to slic for test execution

--- tests/config.php
+++ tests/config.php
@@ -1,20 +1,11 @@
 <?php
 $slic = getenv( 'STELLAR_SLIC' );
 
-// Set up differently if running in slic
-if ( ! empty( $slic ) ) {
-	// Paths
-	$constants = [
-		'WP_CONTENT_DIR' => getenv( 'WP_ROOT_FOLDER' ) . '/wp-content',
-		'ABSPATH'        => getenv( 'WP_ROOT_FOLDER' ) . '/',
-	];
-} else {
-	// Paths
-	$constants = [
-		'WP_CONTENT_DIR' => dirname( __FILE__ ) . '/../../../../wp-content',
-		'ABSPATH'        => dirname( __FILE__ ) . '/../../../../wp',
-	];
-}
+// Paths
+$constants = [
+	'WP_CONTENT_DIR' => getenv( 'WP_ROOT_FOLDER' ) . '/wp-content',
+	'ABSPATH'        => getenv( 'WP_ROOT_FOLDER' ) . '/',
+];
 
 foreach ( $constants as $key => $value ) {
 	if ( defined( $key ) ) {
